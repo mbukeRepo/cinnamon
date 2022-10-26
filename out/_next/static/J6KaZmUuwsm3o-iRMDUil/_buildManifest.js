27a504cad021a0f0d3560f9eb3667ea6b4d1fa05
mbukerepo <mbuke404@gmail.com>
2022-10-26 05:41:00 +0200

    submitting production build

---
+++ out/_next/static/J6KaZmUuwsm3o-iRMDUil/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-af3e045524878e14.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/about":["static/chunks/pages/about-c84bdc627388fd0f.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
